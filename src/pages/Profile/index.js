e6d50d376fe781b36a0255f2d9944be0bfac0800
Rafael <rafaelbpa@gmail.com>
2019-08-05 14:20:36 -0300

    Styling routes (bar navigation)

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
-
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
-export default function Dashboard() {
+export default function Profile() {
   return <Background />;
 }
 
-Dashboard.navigationOptions = {
-  tabBarLabel: 'Meetups',
+Profile.navigationOptions = {
+  tabBarLabel: 'Perfil',
   tabBarIcon: ({ tintColor }) => (
-    <Icon name="list" size={20} color={tintColor} />
+    <Icon name="person" size={20} color={tintColor} />
   ),
 };
--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,9 @@ import {
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 export default (isSigned = false) =>
   createAppContainer(
@@ -16,9 +18,22 @@ export default (isSigned = false) =>
           SignIn,
           SignUp,
         }),
-        App: createBottomTabNavigator({
-          Dashboard,
-        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#FFF',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              style: {
+                backgroundColor: '#2B1A2F',
+              },
+            },
+          }
+        ),
       },
       {
         initialRouteName: isSigned ? 'App' : 'Sign',
